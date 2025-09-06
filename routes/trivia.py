3615e1ad0ebf8360105d67c5f64cf8360e3970aa
William Chia <wc1422@ic.ac.uk>
2025-09-06 20:12:17 +0800

    trivia q2 3

--- routes/trivia.py
+++ routes/trivia.py
@@ -4,9 +4,9 @@ from routes import app
 @app.route('/trivia', methods=['GET'])
 def trivia():
     answers = [
-        3  # Q1: "Trivia!" → 1 challenge ending with "!"
-        # 1,  # Q2: "Ticketing Agent" → Concert
-        # 2,  # Q3: "Blankety Blanks" → 100 lists x 1000 elements
+        3,  # Q1: "Trivia!" → 1 challenge ending with "!"
+        1,  # Q2: "Ticketing Agent" → Concert
+        2,  # Q3: "Blankety Blanks" → 100 lists x 1000 elements
         # 2,  # Q4: "Princess Diaries" → Fat Louie
         # 3,  # Q5: "MST Calculation" → 8
         # 4,  # Q6: "UBS Surveillance" → Amy Winehouse
